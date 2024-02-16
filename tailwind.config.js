3688201abbaf6f1c3855611a59a7f95481ed43ca
Jorge Luis Sanchez Mendoza <57418730+jorgeula300@users.noreply.github.com>
2024-02-15 20:43:54 -0500

    corrijo portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class','[data-theme = dark]'],
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
