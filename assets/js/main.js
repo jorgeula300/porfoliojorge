f1caf459c125dedbcae346533c6740be51dc3f87
Jorge Luis Sanchez Mendoza <57418730+jorgeula300@users.noreply.github.com>
2024-02-15 21:36:31 -0500

    dark mode corregido

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,15 @@
 import cerrarMenu from "./helpers/abrir.js";
+import darkEs from "./helpers/darkEs.js";
 import sendEmail from "./helpers/enviarMensaje.js";
 
-
+darkEs()
 cerrarMenu()
 sendEmail()
 
 
-const pagina = document;
 
 
 
-pagina.addEventListener('DOMContentLoaded', () => {
-    const switctherTheme = document.querySelector('#darkmode-toggle');
-    const root = document.documentElement;
-    const mql = window.matchMedia('(prefers-color-scheme: dark)');
-    console.log(mql)
 
-
-    // con esta funcion me doy cuenta si el usuario esta en modo oscuro o no dependiendo la configuracion de su navegador 
-    function handleMediaQueryChange(e) {
-        if (e.matches) {
-            // Activar el modo oscuro
-            switctherTheme.checked = true;
-            root.setAttribute('data-theme', "dark");
-        } else {
-            // Desactivar el modo oscuro
-            switctherTheme.checked = false;
-            root.setAttribute('data-theme', 'light');
-        }
-    }
-
-
-    handleMediaQueryChange(mql);
-
-
-    // con esta funcion cambio el tema dependiendo si el usuario esta en modo oscuro o no con el check 
-    switctherTheme.addEventListener('click', () => {
-        const setTheme = switctherTheme.checked ? 'dark' : 'light';
-        root.setAttribute('data-theme', setTheme);
-        window.localStorage.setItem('theme', setTheme); // Guardar el tema en el almacenamiento local
-    });
-
-    // establece el tema que el sistema haya guardado en el locla storage
-    const theme = window.localStorage.getItem('theme');
-    const systemTheme = mql.matches ? 'dark' : 'light';
-    const newTheme = theme ?? systemTheme;
-    pagina.documentElement.setAttribute('data-theme', newTheme);
-
-})
 
 
